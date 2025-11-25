405f93702b0f7779ceb0e28fd939746124152504
Jadon Zufall <jadonzufall@gmail.com>
2025-11-24 19:38:58 -0500

    Added comments
    Added comments to explain when each function is called.

--- Addons/example_plugin/example_plugin.gd
+++ Addons/example_plugin/example_plugin.gd
@@ -2,11 +2,11 @@
 extends EditorPlugin
 
 
-func _enter_tree() -> void:
+func _enter_tree() -> void:                                                                         # Called when plugin is enabled.
 	# Initialization of the plugin goes here.
 	pass
 
 
-func _exit_tree() -> void:
+func _exit_tree() -> void:                                                                          # Called when plugin is disabled.
 	# Clean-up of the plugin goes here.
 	pass
