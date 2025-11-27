ce675ed1d5cde39510f3b2761310b6f400c5515e
Jadon Zufall <jadonzufall@gmail.com>
2025-11-26 21:56:23 -0500

    Add Greybox Editor Godot plugin
    Introduces a new 'Greybox Editor' Godot plugin for editing greybox levels, including scripts, scenes, shaders, icons, and configuration. Updates project settings to enable the plugin and sets default viewport size. Removes unused mesh exports from Greybox Blender file and cleans up related resources. Minor update to scene_lobby.tscn to add a 'level_data' node.

---
+++ Addons/greybox_editor/screenshot.gd
@@ -0,0 +1,31 @@
+@tool
+extends Button
+
+@export var sub_viewport: SubViewport
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_pressed() -> void:
+	print("Screenshot")
+	var screen_shot_w: int = 256
+	var screen_shot_h: int = 256
+	var image: Image = sub_viewport.get_texture().get_image()
+	print(image.get_size())
+	var p1: Vector2 = Vector2(
+		image.get_width() / 2 - screen_shot_w / 2,
+		image.get_height() / 2 - screen_shot_h / 2
+	)
+	var p2: Vector2 = Vector2(
+		image.get_width() / 2 + screen_shot_w / 2,
+		image.get_height() / 2 + screen_shot_h / 2
+	)
+	image = image.get_region(Rect2(p1, p2 - p1))
+	image.save_png("res://screenshot.png")
