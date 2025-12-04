ea155c9a22649c21f85d4c0a326324b4b9423f43
Jadon Zufall <jadonzufall@gmail.com>
2025-12-04 01:44:20 -0500

    dedicated server

--- Game/Scenes/scene_loader/scripts/scene_loader.gd
+++ Game/Scenes/scene_loader/scripts/scene_loader.gd
@@ -1,14 +1,24 @@
 @icon("res://Editor/Icons/PackedScene.svg")
 extends Node
-@onready var world_ui: SceneTreeUI = $UI
+@onready var ui_loader: SceneTreeUI = $"UI Loader"
 @onready var world_2d: SceneTree2D = $"2D World"
 @onready var world_3d: SceneTree3D = $"3D World"
 
+
+@export var server_debugger: PackedScene
 @export var autoload_ui: Array[PackedScene] = []
 @export var autoload_2d: Array[PackedScene] = []
 @export var autoload_3d: Array[PackedScene] = []
 
 
 func _ready() -> void:
+	var args = Array(OS.get_cmdline_args())
+	
+	# If the flags contain -s run as server
+	if args.has("-s"):
+		ui_loader.add_child(server_debugger.instantiate())
+		Network.sv_open()
+	
+	
 	for scene in autoload_ui:
-		world_ui.add_child(scene.instantiate())
+		ui_loader.add_child(scene.instantiate())
