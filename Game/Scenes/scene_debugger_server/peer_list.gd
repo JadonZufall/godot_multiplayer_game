619f5070177e6806e7e6f40be5d462843fecf388
Jadon Zufall <jadonzufall@gmail.com>
2025-12-05 18:19:22 -0500

    added support for displaying usernames in peer_list

--- Game/Scenes/scene_debugger_server/peer_list.gd
+++ Game/Scenes/scene_debugger_server/peer_list.gd
@@ -1,20 +1,30 @@
 extends VBoxContainer
 
 
-var peers: Dictionary[int, Node] = {}
+var peers: Dictionary[int, Label] = {}
 
 func _ready() -> void:
 	Network.sv_peer_connected.connect(_on_peer_connected)
 	Network.sv_peer_disconnected.connect(_on_peer_disconnected)
+	Network.sv_peer_set_username.connect(_on_peer_set_username)
 
 func _on_peer_connected(pid: int) -> void:
 	var label: Label = Label.new()
 	add_child(label)
-	label.text = str(pid)
+	label.text = "[%d] %s" % [pid, "<null>"]
 	label.name = str(pid)
-	peers[pid] = label
+	peers.set(pid, label)
 
 func _on_peer_disconnected(pid: int) -> void:
-	var target: Label = peers.get(pid)
-	if target:
-		target.queue_free()
+	var label: Label = peers.get(pid)
+	if not label:
+		push_error("Unable to locate label for [%d] %s" % [pid, "<null>"])
+		return
+	label.queue_free()
+
+func _on_peer_set_username(pid: int, username: String) -> void:
+	var label: Label = peers.get(pid)
+	if not label:
+		push_error("Unable to locate label for [%d] %s" % [pid, username])
+		return
+	label.text = "[%d] %s" % [pid, username]
