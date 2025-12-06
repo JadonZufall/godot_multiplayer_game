cfb65f1576b0e5167243e330089b29d2405b64d1
Jadon Zufall <jadonzufall@gmail.com>
2025-12-05 19:05:51 -0500

    peer list displays connected peers (sometimes)

--- Game/Scenes/scene_debugger_client/peer_list.gd
+++ Game/Scenes/scene_debugger_client/peer_list.gd
@@ -1,12 +1,23 @@
 extends VBoxContainer
 
+var _peers: Dictionary[int, Label] = {}
+
 
 func _ready() -> void:
 	Network.cl_peer_connected.connect(_on_peer_connected)
 	Network.cl_peer_disconnected.connect(_on_peer_disconnected)
 
 func _on_peer_connected(pid: int) -> void:
-	pass
+	var label: Label = Label.new()
+	add_child(label)
+	label.text = "[%d] %s" % [pid, "<null>"]
+	label.name = str(pid)
+	_peers.set(pid, label)
 
 func _on_peer_disconnected(pid: int) -> void:
-	pass
+	if not _peers.has(pid):
+		Network.cout("No peer found [%d] %s" % [pid, "<null>"])
+		push_error("No peer found [%d] %s" % [pid, "<null>"])
+		return
+	var label: Label = _peers[pid]
+	label.queue_free()
