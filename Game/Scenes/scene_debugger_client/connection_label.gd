d384676cfecb5a698c7072db4deb42bb526755c3
Jadon Zufall <jadonzufall@gmail.com>
2025-12-05 19:35:19 -0500

    Clients own username is displayed

--- Game/Scenes/scene_debugger_client/connection_label.gd
+++ Game/Scenes/scene_debugger_client/connection_label.gd
@@ -4,9 +4,15 @@ extends Label
 func _ready() -> void:
 	Network.cl_join.connect(_on_join)
 	Network.cl_exit.connect(_on_exit)
+	Network.cl_peer_set_username.connect(_on_peer_set_username)
+
+func _on_peer_set_username(pid: int, username: String) -> void:
+	if Network.local_pid != pid:
+		return
+	text = "Connected as [%d] %s" % [Network.local_pid, username]
 
 func _on_join() -> void:
-	text = "Connected as %d" % Network.local_pid
+	text = "Connected as [%d] %s" % [Network.local_pid, "<null>"]
 
 func _on_exit() -> void:
 	text = "Disconnected"
