6de6b30c8fe2b6a4e0667e82c5dc277da9a6e2f4
Jadon Zufall <jadonzufall@gmail.com>
2025-12-07 21:44:53 -0500

    Player Service

---
+++ Services/PlayerService/PlayerService.gd
@@ -0,0 +1,12 @@
+extends Node
+
+
+func _ready() -> void:
+	Network.sv_peer_connected.connect(_on_player_join)
+	Network.sv_peer_disconnected.connect(_on_player_quit)
+
+func _on_player_join() -> void:
+	pass
+
+func _on_player_quit() -> void:
+	pass
