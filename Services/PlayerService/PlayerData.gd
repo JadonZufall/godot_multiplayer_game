9ebe1dfab3af5412439c37c25dae43f3e657f24b
Jadon Zufall <jadonzufall@gmail.com>
2025-12-09 17:44:42 -0500

    minor changes towards a uuid direction to allow for reconnection of players.

--- Services/PlayerService/PlayerData.gd
+++ Services/PlayerService/PlayerData.gd
@@ -1,4 +1,53 @@
 class_name PlayerData extends Node
 
+const TOKEN_LEN: int = 64
+const TOKEN_UNIX_PADDING: int = 12
+const TOKEN_UNIX_DATE_FORMAT: String = "%012.0f"
+const TOKEN_DELIMITER: String = "|"
+const TOKEN_EXPIRE_SECONDS: int = 0
+const TOKEN_EXPIRE_MINUTES: int = 0
+const TOKEN_EXPIRE_HOURS: int = 0
+const TOKEN_EXPIRE_DAYS: int = 1
 
-var pid: int
+signal player_join
+signal player_quit
+
+var pid: int = -1
+var uuid: String
+var username: String
+var token: String
+var is_connected: bool:
+	get: return pid != -1
+
+static func unix_delta_time(days: int, hours: int, minutes: int, seconds: int) -> int:
+	var result: int = days
+	result *= 24  # 24 hours in a day
+	result += hours
+	result *= 60  # 60 minutes in an hour
+	result += minutes
+	result *= 60  # 60 seconds in a minute
+	result += seconds
+	return result
+
+static func validate_token(token: String) -> Dictionary:
+	var data: Array[String] = token.split(TOKEN_DELIMITER)
+	return {}
+
+static func generate_token(username: String) -> String:
+	var result: String = ""
+	
+	var issue_date: float = Time.get_unix_time_from_system()
+	var expire_date: float = issue_date + unix_delta_time(TOKEN_EXPIRE_DAYS, TOKEN_EXPIRE_HOURS, TOKEN_EXPIRE_MINUTES, TOKEN_EXPIRE_SECONDS)
+	
+	result += TOKEN_UNIX_DATE_FORMAT % issue_date
+	result += TOKEN_DELIMITER
+	result += TOKEN_UNIX_DATE_FORMAT % expire_date
+	result += TOKEN_DELIMITER
+	result += username
+	result += TOKEN_DELIMITER
+	
+	var rand: String = ""
+	for i in range(TOKEN_LEN):
+		pass
+	
+	return result
--- Services/PlayerService/PlayerService.gd
+++ Services/PlayerService/PlayerService.gd
@@ -3,17 +3,96 @@ extends Service
 signal player_join(data: PlayerData)
 signal player_left(data: PlayerData)
 
+var _pdata: Dictionary[String, PlayerData] = {}
+var _Random: RandomNumberGenerator = RandomNumberGenerator()
+
+static func generate_hex(len: int) -> String:
+	var result: String = ""
+	for i in range(len):
+		if _Random.randf() < 0.5:
+			result += char(_Random.randi_range(48, 57))
+		else:
+			result += char(_Random.randi_range(97, 102))
+	return result
+
+static func generate_uuid_v4() -> String:
+	# 8-4-4-4-12
+	return "%s-%s-%s-%s-%s" % [generate_hex(8), generate_hex(4), generate_hex(4), generate_hex(4), generate_hex(12)]
+
+func _pdata_clear() -> void:
+	_pdata.clear()
 
 func _ready() -> void:
+	Network.sv_host.connect(_on_server_host)
+	Network.sv_exit.connect(_on_server_exit)
+	Network.cl_join.connect(_on_client_join)
+	Network.cl_exit.connect(_on_client_exit)
+
+func _error_log(message: String) -> void:
+	Network.cout("[ERROR] (PlayerService) %s" % message)
+
+func _error_listeners_already_connected() -> void: return _error_log("Unabled to bind listeners for, listeners already connected.")
+func _error_no_player_data(pid: int=-1) -> void: return _error_log("No player_data for %d" % pid)
+func _error_duplicate_player_data(pid: int=-1) -> void: return _error_log("Duplicate player_data for %d" % pid)
+func _error_already_disconnected(pid: int=-1) -> void: return _error_log("Failed to disconnect player_data for %d, player was already disconnected." % pid)
+
+func _is_network_listeners_connected() -> bool:
+	var result: bool = false
+	result = result or Network.sv_peer_connected.is_connected(_on_player_join)
+	result = result or Network.sv_peer_disconnected.is_connected(_on_player_left)
+	result = result or Network.cl_peer_connected.is_connected(_on_player_join)
+	result = result or Network.cl_peer_disconnected.is_connected(_on_player_left)
+	return result
+
+func _on_server_host() -> void:
+	if _is_network_listeners_connected():
+		return _error_listeners_already_connected()
+	_pdata_clear()
 	Network.sv_peer_connected.connect(_on_player_join)
 	Network.sv_peer_disconnected.connect(_on_player_left)
 
+func _on_server_exit() -> void:
+	if _is_network_listeners_connected():
+		return _error_listeners_already_connected()
+	_pdata_clear()
+	Network.sv_peer_connected.disconnect(_on_player_join)
+	Network.sv_peer_disconnected.disconnect(_on_player_left)
+
+func _on_client_join() -> void:
+	if _is_network_listeners_connected():
+		return _error_listeners_already_connected()
+	_pdata_clear()
+	Network.cl_peer_connected.connect(_on_player_join)
+	Network.cl_peer_disconnected.connect(_on_player_left)
+
+func _on_client_exit() -> void:
+	if _is_network_listeners_connected():
+		return _error_listeners_already_connected()
+	_pdata_clear()
+	Network.cl_peer_connected.disconnect(_on_player_join)
+	Network.cl_peer_disconnected.connect(_on_player_left)
+
 func _on_player_join(pid: int) -> void:
-	var player_data: PlayerData = PlayerData.new()
+	var player_data: PlayerData
+	if _pdata.has(pid):
+		if _pdata[pid].is_connected: return _error_duplicate_player_data(pid)
+	
+	player_data = PlayerData.new()
+	player_data.uuid = generate_uuid_v4()
+	_pdata[player_data.uuid] = player_data
+	
 	player_data.pid = pid
 	add_child(player_data)
 	player_join.emit(player_data)
+	player_data.player_join.emit(player_data)
 
-func _on_player_left() -> void:
+func _on_player_left(pid: int) -> void:
+	if not _pdata.has(pid): return _error_no_player_data(pid)
 	
-	player_left.emit()
+	var player_data: PlayerData = _pdata[pid]
+	if not player_data.is_connected: return _error_already_disconnected(pid)
+	
+	player_data.pid = -1
+	remove_child(player_data)
+	player_left.emit(player_data)
+	player_data.player_quit.emit(player_data)
